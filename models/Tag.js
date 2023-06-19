67b859bf0b9b582f11276afa9ce4ed5f2f683047
Fergus Illman <fergus.illman.au@gmail.com>
2023-06-19 21:22:30 +0930

    Added routes & video walkthrough

--- models/Tag.js
+++ models/Tag.js
@@ -10,11 +10,11 @@ Tag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
-   }, 
-   tag_name: {
+      autoIncrement: true
+    },
+    tag_name: {
       type: DataTypes.STRING,
-   }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,29 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-})
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
-
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete:'CASCADE'
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
-
 Product.belongsToMany(Tag, {
-  through: "product_tag"
-})
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
-
 Tag.belongsToMany(Product, {
-  through: "product_tag"
-})
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
